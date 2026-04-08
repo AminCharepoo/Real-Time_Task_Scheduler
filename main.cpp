1c99b9e5dac511a438954b16a5b960b5ee9cac4b
Dani Robinson <robinson.reb@northeastern.edu>
2026-04-08 00:51:39 -0400

    Move edge cases to new file, simplifying main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "TimerTask.h"
-#include "ButtonTask.h"
-#include "DisplayTask.h"
+#include "Task.h"
+#include "Cases.h"
 #include "utils.h"
+
+// CONFIG (DEFAULT_MODE, EDGE_CASE_1, EDGE_CASE_2, or EDGE_CASE_3)
+const CaseMode CURRENT_MODE = DEFAULT_MODE;
 
 int main()
 {
     stdio_init_all();
-    sleep_ms(3000); // wait for 2 seconds to allow time for the USB connection to be established
+    sleep_ms(3000);
 
     printf("System Ready\n");
 
@@ -19,39 +21,7 @@ int main()
 
     Queue queue;
 
-    TimerTask timer1("Timer1", LOW, DROP, 500, toggle_led1, queue);
-    ButtonTask button("Button1", HIGH, FRONT_QUEUE, 11, toggle_led2, queue);
-    /*// Commeneted out to run edge cases
-    DisplayTask display("Display1", LOW, FRONT_QUEUE, "Hello, World! ", 3, 300, queue);
-    */
-    DisplayTask display("Display1", HIGH, FRONT_QUEUE, "Hello, World! ", 16, 300,queue);
-
-    ButtonTask button2("Button2", VIP, FRONT_QUEUE, 9, [&display]() {display.displayVipMessage("VIP Message!"); }, queue);
-
-    /*
-    // Edge Case 1: Long message that exceeds LCD screen size to test proper message handling
-    DisplayTask longDisplay("LongDisplay", LOW, FRONT_QUEUE,
-        "THIS MESSAGE IS TOO LONG FOR THE LCD SCREEN", 3, 300, queue);
-    
-        ButtonTask button2("Button2", VIP, FRONT_QUEUE, 9, [&longDisplay]() {longDisplay.displayVipMessage("VIP Message!"); }, queue);
-    */
-
-    /*
-    // Edge Case 2: More timer tasks to help stress the queue
-    TimerTask timer2("Timer2", LOW, DROP, 300, toggle_led1, queue);
-    TimerTask timer3("Timer3", LOW, DROP, 200, toggle_led2, queue);
-    */
-    
-
-    timer1.start();
-    button.setup();
-    display.setup();
-    button2.setup();
-    //display.setup(); // edge case 1
-   // button2.setup(); // edge case 1
-    //longDisplay.setup(); // edge case 1
-    //timer2.start(); // edge case 2
-    //timer3.start(); // edge case 2
+    setupCase(CURRENT_MODE, queue);
 
     printf("Setup complete\n");
 
@@ -60,27 +30,27 @@ int main()
     while (true) {
         Task* t = queue.dequeue();
 
-        // Edge case 3: empty task queue
         if (t == nullptr) {
             printf("Queue empty - idle state\n");
             tight_loop_contents();
             continue;
         }
 
-        if (t){
-            t->execute();
-            if (queue.isVipPending() && t->getInterruptBehavior() == FRONT_QUEUE) { interrupted_task = t; } // if there is a VIP task waiting and the current task should be requeued at front store it in interrupted task
+        t->execute();
 
-            if (t->getPriority() == VIP) {
-                queue.clearVipPending(); // clear VIP flag if we just executed a VIP task
-                if (interrupted_task) { // if there is an interrupted task requeue it in the front
-                    queue.push_front(interrupted_task);
-                    interrupted_task = nullptr;
-                }
+        if (queue.isVipPending() && t->getInterruptBehavior() == FRONT_QUEUE) {
+            interrupted_task = t;
+        }
+
+        if (t->getPriority() == VIP) {
+            queue.clearVipPending();
+
+            if (interrupted_task) {
+                queue.push_front(interrupted_task);
+                interrupted_task = nullptr;
             }
         }
-        else tight_loop_contents(); // if no tasks to execute, run tight loop to save power
     }
 
     return 0;
-};
+}
